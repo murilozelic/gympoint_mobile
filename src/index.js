80d6679d150b9adc9d51b949cc4dd3e1dbca022f
Murilo Zelic <murilozelic@gmail.com>
2020-04-29 10:57:54 -0300

    Default Project RN

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function src() {
+  return <View />;
+}
